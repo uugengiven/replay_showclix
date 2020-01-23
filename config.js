d5eed6c9b9aed823b41a97422629f73862d4cce8
John Lange <lange.john.m@gmail.com>
2020-01-23 17:48:51 -0500

    Change env var to DB_HOST

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {}
 
-config.host = process.env.HOST;
+config.host = process.env.DB_HOST;
 config.authKey = process.env.AUTHKEY;
 config.databaseId = process.env.DB_ID;
 config.collectionId = process.env.COLLECTION_ID;
